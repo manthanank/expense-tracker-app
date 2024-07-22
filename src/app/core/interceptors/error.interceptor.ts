c51fa4d4e66248457abfcbb9612094f68133e300
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-07-22 21:15:08 +0530

    chore: Inject Router and redirect to login on logout

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -2,13 +2,16 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const ErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router); // Inject Router
   return next(req).pipe(
     catchError((error) => {
       if (error.status === 401 || error.status === 403 || error.status === 404) {
         authService.logout();
+        router.navigate(['/login']); // Redirect to login after logout
       }
       return throwError(() => error);
     })
