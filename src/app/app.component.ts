193828596e9f0868541adaa80eead4da84921942
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-03 11:45:03 +0530

    chore: Add meta tags for viewport, icon, canonical, og:title, author, keywords, robots, og:description, og:image, and og:url in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'expense-tracker-app';
+
+  meta = inject(Meta);
+
+  constructor() {
+    this.meta.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1' });
+    this.meta.addTag({ name: 'icon', content: 'image/x-icon', href: 'favicon.ico' });
+    this.meta.addTag({ name: 'canonical', content: 'https://expense-tracker-app-manthanank.vercel.app/' });
+    this.meta.addTag({ property: 'og:title', content: 'Expense Tracker App' });
+    this.meta.addTag({ name: 'author', content: 'Manthan Ankolekar' });
+    this.meta.addTag({ name: 'keywords', content: 'angular, nodejs. express, mongodb' });
+    this.meta.addTag({ name: 'robots', content: 'index, follow' });
+    this.meta.addTag({ property: 'og:description', content: 'A simple expense tracker app, built with Angular. It helps you to track your expenses. You can add, edit, delete, and filter your expenses.' });
+    this.meta.addTag({ property: 'og:image', content: 'https://expense-tracker-app-manthanank.vercel.app/image.jpg' });
+    this.meta.addTag({ property: 'og:url', content: 'https://expense-tracker-app-manthanank.vercel.app/' });
+  }
 }
