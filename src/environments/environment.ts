342503859e4ebc11ebefd88d378d5f75885114d4
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-10-16 20:59:15 +0530

    chore: Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl:
     window.location.hostname === 'localhost'
-      ? 'https://expense-tracker-app-8y8i.vercel.app/api'
+      ? 'http://localhost:3000/api'
       : 'https://expense-tracker-app-8y8i.vercel.app/api',
 };
