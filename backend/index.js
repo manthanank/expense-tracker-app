5953e0700e2401b28f9630d4b208eb0d97e52417
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-04-10 09:28:33 +0530

    feat: Update environment variable references for frontend and backend URLs

--- backend/index.js
+++ backend/index.js
@@ -86,6 +86,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(
-    `Swagger documentation available at ${process.env.BASE_URL}/api-docs`
+    `Swagger documentation available at ${process.env.BACKEND_URL}/api-docs`
   );
 });
